8471a9e80e6bb1592dacd02731767f986a5ec68b
RakshitaaGM <rakshitaagm@gmail.com>
2022-07-05 22:25:08 -0500

    Added description

--- RunningSumOf1DArray.cpp
+++ RunningSumOf1DArray.cpp
@@ -1,3 +1,10 @@
+//Source : https://leetcode.com/problems/running-sum-of-1d-array/
+//Author : Rakshitaa Geetha Mohan
+//email ID : rakshitaagm@gmail.com
+//Date : 07/04/2022
+//Difficulty level : Easy
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Description
 // Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 // Return the running sum of nums.
 // Example 1:
